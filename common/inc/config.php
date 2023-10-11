c6fcfb75a60d9f591e9608f04ec096f798720bd0
shriradharani <brij.phpdev@gmail.com>
2023-10-11 23:00:18 +0530

    update config

---
+++ common/inc/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+define('SITE_URL','https://empire.theroyal.org.in');
+define('SITE_BOOK_URL','https://empirebook.theroyal.org.in');
+define('EMAIL','mb.consulting.031220@gmail.com');
+define('PHONE','0562 400 0611');
+define('ADDRESS','2nd Floor, Shanti Emporio, Ramnagar Colony, Civil Lines, Agra, Uttar Pradesh 282002');
+
+define('INSTA_SOCIAL','https://www.instagram.com/empiresalonofficial/?hl=en');
+define('FB_SOCIAL','https://www.facebook.com/empireagraindia/');
+
+define('DB_HOST','62.72.28.103');
+define('DB_USER_NAME','u963541063_salon_bkng_usr');
+define('DB_USER_PASSWORD','8}_!.)%%Atnh65@11Oct23');
+define('DB_NAME','u963541063_salon_booking');
--- common/inc/header.php
+++ common/inc/header.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ALL);
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
