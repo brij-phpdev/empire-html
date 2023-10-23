a106857c218460777777e1ced4f681ee1c89a2c2
shriradharani <brij.phpdev@gmail.com>
2023-10-23 19:05:54 +0530

    franchise page

--- franchise.php
+++ franchise.php
@@ -1,145 +1,117 @@
 <?php
 include_once './common/inc/header.php';
-//include_once './common/inc/database.php';
 ?>
 
-<!-- content begin -->
-<div id="content" class="no-bottom no-top">
-
-    
-
-   
-
-   
-
-    
-
-    <section id="explore-4" class="side-bg text-light">
-        <div class="col-md-6 image-container">
-            <div class="background-image"></div>
+<!-- subheader -->
+<section id="subheader" class="subh-center" data-stellar-background-ratio=".2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Franchise</h1>
+                <h4> lorum ippsum</h4>
+            </div>
         </div>
+    </div>
+</section>
+<!-- subheader close -->
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-offset-7">
-                    <div class="padding40">
-                        <h2>Franchise
-                            <span class="small-border"></span>
-                        </h2>
-                        Are you new to this business & seeking the
+<!-- content begin -->
+<div id="content" class="no-top">
 
-                        <div class="spacer-single"></div>
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h4>Experience</h4>
-                                <p>With years of experiences and continuing education, our dedicated staff is ready to serve your beauty needs.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center">
+                <h2>Fill the form we will get back to you</h2>
+                <div class="small-border wow zoomIn" data-wow-delay=".3s" data-wow-duration=".3s"></div>
+            </div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <form name="jobForm" id='job_form' class="deform-1" method="post" action=''>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div id='name_error' class='error'>Please enter your name.</div>
+                            <div>
+                                <input type='text' name='name' id='name' class="form-control" placeholder="Candidate Name">
                             </div>
 
+                            <div id='email_error' class='error'>Please enter your valid E-mail ID.</div>
+                            <div>
+                                <input type='text' name='email' id='email' class="form-control" placeholder="Candidate Email">
+                            </div>
+
+                            <div id='phone_error' class='error'>Please enter your phone number.</div>
+                            <div>
+                                <input type='text' name='phone' id='phone' class="form-control" placeholder="Your Phone">
+                            </div>
+                            
                             
                         </div>
-                    </div>
+                        
+                                <div class="col-md-6">
+                                <select name='experience_year' id='experience_year' class="form-control" placeholder="Years of experience">
+                                    <?php foreach($experience_years as $experience_year): ?>
+                                    <option value="<?php echo $experience_year ?>"><?php echo $experience_year ?></option>
+                                    <?php endforeach; ?>
+                                </select><label for="years"> Years</label>
+                                </div>
+                                <div class="col-md-6">
+                                <select name='experience_month' id='experience_month' class="form-control">
+                                    <?php foreach($experience_months as $experience_month): ?>
+                                    <option value="<?php echo $experience_month ?>"><?php echo $experience_month ?></option>
+                                    <?php endforeach; ?>
+                                </select> Months
+                                </div>
+                                Leave blank in case you are fresher
+                            
 
-                </div>
-            </div>
-        </div>
-    </section>
 
-
-    <!-- section begin -->
-    <section id="section-gallery">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3 text-center">
-                    <h1>Hair Styles</h1>
-                    <div class="small-border wow zoomIn" data-wow-delay=".3s" data-wow-duration=".3s"></div>
-                    <div class="spacer-single"></div>
-                </div>
-            </div>
-        </div>
-
-        <div class="container">
-
-            <div id="gallery" class="gallery zoom-gallery wow fadeInUp gallery-col" data-wow-delay=".3s">
-                <div class="row">
-
-                    <?php
-                    $gallery_dir = 'images/gallery/';
-                    
-                    if(is_dir( $gallery_dir)):
-                            $gallery_files = array_diff(scandir($gallery_dir), array('.', '..','.DS_Store'));
-//                            shuffle($gallery_files);
-                            if (count($gallery_files)):
-                                foreach ($gallery_files as $k => $gallery_file):
+                        <div class="col-md-12">
+                            <div id='expertise_error' class='error'>Please enter your expertise such as hair stylist, beautician, etc.</div>
+                            <div>
+                                <input type='text' name='expertise' id='expertise' class="form-control" placeholder="Candidate Expertise">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div id='reference_error' class='error'>Please enter your Reference (if Any).</div>
+                            <div>
+                                <input type='text' name='reference' id='reference' class="form-control" placeholder="Candidate Reference">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div id='source_error' class='error'>Source of careers.</div>
+                            <div>
+                                <select name='source' id='source' class="form-control" placeholder="Career Source, such as ">
+                                    <option value="0">--Select One--</option>
+                                    <?php foreach($career_source_references as $career_source_reference): ?>
+                                    <option value="<?php echo $career_source_reference ?>"><?php echo $career_source_reference ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
                                 
-                                    ?>
-
-                    <!-- gallery item -->
-                    <div class="col-md-4 item">
-                        <div class="picframe">
-                            <a href="<?php echo $gallery_dir . $gallery_file ?>" title="Hair Style 1">
-                                <span class="overlay">
-                                    <span class="pf_text">
-                                        <span class="project-name">Style 1</span>
-                                    </span>
-                                </span>
-                                <img src="<?php echo $gallery_dir . $gallery_file ?>" alt="" />
-                            </a>
+                       <div class="col-md-12">
+                            <div id='resume_error' class='error'>Upload correct file & try again</div>
+                            <div>
+                                <input type="file" name="cadidate_resume" />
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <p id='submit'>
+                                <input disabled="disabled" type='submit' id='send_message' value='Submit Form' class="btn btn-custom">
+                            </p>
+                            <div id='mail_success' class='success'>Your message has been sent successfully.</div>
+                            <div id='mail_fail' class='error'>Sorry, error occured this time sending your message.</div>
                         </div>
                     </div>
-                    <?php
-                                endforeach;
-                            endif;
-                        endif;
-                        ?>
-                    <!-- close gallery item -->
+                </form>
 
-                    
-                    
-                </div>
             </div>
+
+
         </div>
-    </section>
-    <!-- section close -->
-
-
-    <!-- section begin -->
-    <section id="section-home-shop"  class="side-bg text-light" aria-label="section-services" data-stellar-background-ratio=".2">
-        <div class="col-md-6 image-container">
-            <div class="background-image"></div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-offset-7">
-                    <div class="padding40">
-                        <h2>Grab your deal
-                            <span class="small-border"></span>
-                        </h2>
-                        Claim your Welcome bonus by becoming a part of our community, and continue shopping for products from our store.
-                        <br/>
-                        <a target="_blank" href="<?php echo SHOP_URL ?>" class="btn btn-line-white btn-big">Shop Now</a>
-
-                        <div class="spacer-single"></div>
-
-
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- section close -->
-
-
-    <!-- section begin -->
-    <section id="cta" aria-label="cta" class="call-to-action bg-color-2 text-center">
-        <a href="services.php" class="btn btn-line-white btn-big">Make Appointment Now</a>
-    </section>
-    <!-- section close -->
-
-
+    </div>
 </div>
-
 
 <!-- footer begin -->
 <?php
