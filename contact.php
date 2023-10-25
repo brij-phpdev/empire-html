a48097650099305574c5c25dd70e76ecbd2ceea7
shriradharani <brij.phpdev@gmail.com>
2023-10-25 15:13:55 +0530

    update contact us page

--- contact.php
+++ contact.php
@@ -1,80 +1,123 @@
 <?php
 include_once './common/inc/header.php';
+include_once './common/inc/database.php';
 ?>
-
-<!-- subheader -->
-<section id="subheader" class="subh-center" data-stellar-background-ratio=".2">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Contact</h1>
-                <h4>Get In Touch</h4>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- subheader close -->
-
-<!-- content begin -->
-<div id="content" class="no-top">
-
-    <!-- section begin -->
-<!--    <section id="section-map" aria-label="section-map" class="no-top">
-        <div id="map"></div>
-    </section>-->
-    <!-- section close -->
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center">
-                <h2>Send Us Message</h2>
-                <div class="small-border wow zoomIn" data-wow-delay=".3s" data-wow-duration=".3s"></div>
-            </div>
-
-            <div class="col-md-8 col-md-offset-2">
-                <form name="contactForm" id='contact_form' class="deform-1" method="post" action='email.php'>
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+            <!-- section begin -->
+            <section id="subheader" class="jarallax">
+                <img src="images/background/5.jpg" class="jarallax-img" alt="">
+                <div class="container">
                     <div class="row">
-                        <div class="col-md-4">
-                            <div id='name_error' class='error'>Please enter your name.</div>
-                            <div>
-                                <input type='text' name='name' id='name' class="form-control" placeholder="Your Name">
-                            </div>
-
-                            <div id='email_error' class='error'>Please enter your valid E-mail ID.</div>
-                            <div>
-                                <input type='text' name='email' id='email' class="form-control" placeholder="Your Email">
-                            </div>
-
-                            <div id='phone_error' class='error'>Please enter your phone number.</div>
-                            <div>
-                                <input type='text' name='phone' id='phone' class="form-control" placeholder="Your Phone">
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div id='message_error' class='error'>Please enter your message.</div>
-                            <div>
-                                <textarea name='message' id='message' class="form-control" placeholder="Your Message"></textarea>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 text-center">
-                            <p id='submit'>
-                                <input type='submit' id='send_message' value='Submit Form' class="btn btn-custom">
-                            </p>
-                            <div id='mail_success' class='success'>Your message has been sent successfully.</div>
-                            <div id='mail_fail' class='error'>Sorry, error occured this time sending your message.</div>
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <h1>Contact Us</h1>
+                            <div class="de-separator"></div>
                         </div>
                     </div>
-                </form>
+                </div>
+                <div class="de-gradient-edge-bottom"></div>
+            </section>
+            <!-- section close -->
 
-            </div>
+            <section id="section-content" class="no-top" aria-label="section">
+            <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 text-center">
+                            <i class="fa fa-map-marker de-icon gradient de-hover-scale text-light mb20"></i>
+                            <p class="lead no-bottom">Our Address</p>
+                            <h4 class="s2">100 Mainstreet Center, NY</h4>
+                           
+                        </div>
+                        <div class="col-lg-4 col-md-4 text-center">
+                            <i class="fa fa-phone de-icon gradient de-hover-scale text-light mb20"></i>
+                            <p class="lead no-bottom">Phone Number</p>
+                            <h4 class="s2">+1 333 9296</h4>                                                
+                        </div>
+                        <div class="col-lg-4 col-md-4 text-center">
+                            <i class="fa fa-envelope-o de-icon gradient de-hover-scale text-light mb20"></i>
+                            <p class="lead no-bottom">Email Address</p>
+                            <h4 class="s2">contact@suppport.com</h4>     
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="no-top jarallax">
+                <div class="de-gradient-edge-top"></div>
+                <img src="images/background/7.jpg" class="jarallax-img" alt="">
+                <div class="container position-relative z1000">
+                    <div class="row gx-5">
 
+                        <div class="col-lg-6 offset-lg-3">
 
+                            <div class="d-sch-table">
+                                <h2 class="wow fadeIn text-center">Message Us</h2>
+                                <div class="de-separator"></div>
+                                <p class="lead text-center">
+                                If you got any questions, please do not hestitae to send us a message.
+                                </p>
+
+                                <form name="contactForm" id="contact_form" class="form-border position-relative z1000" method="post" action="#">
+                                    <div class="row">
+                                        <div class="col-lg-12 mb10">
+                                            <div class="field-set">
+                                                <input type="text" name="Name" id="name" class="form-control" placeholder="Your Name" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 mb10">
+                                            <div class="field-set">
+                                                <input type="text" name="Email" id="email" class="form-control" placeholder="Your Email" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 mb10">
+                                            <div class="field-set">
+                                                <input type="text" name="phone" id="phone" class="form-control" placeholder="Your Phone" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                        
+                                    <div class="field-set mb20">
+                                        <textarea name="message" id="message" class="form-control" placeholder="Your Message" required></textarea>
+                                    </div>
+                                    <div class="g-recaptcha" data-sitekey="copy-your-site-key-here"></div>
+                                    <div id='submit' class="mt20">
+                                        <input type='submit' id='send_message' value='Send Message' class="btn-main">
+                                    </div>
+
+                                    <div id="success_message" class='success'>
+                                        Your message has been sent successfully. Refresh this page if you want to send more messages.
+                                    </div>
+                                    <div id="error_message" class='error'>
+                                        Sorry there was an error sending your form.
+                                    </div>
+                                </form>
+
+                                <div class="d-deco"></div>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="de-gradient-edge-bottom"></div>
+            </section>
+            <section aria-label="section" class="no-top sm-hide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <h4>Follow Us</h4>
+                            <div class="spacer-20"></div>
+                            <div class="social-icons big">
+                                <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-linkedin fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-pinterest fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-rss fa-lg"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-    </div>
-</div>
-
-<!-- footer begin -->
+        <!-- content close -->
+        
 <?php
 include_once './common/inc/footer.php';
-?>
